3be76ec7c7172e5c9c897274733208914a11af69
Michael Kriese <michael.kriese@visualon.de>
2025-07-04 09:46:04 +0200

    feat: dotnet wasm (#2)

---
+++ dotnet/Program.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using Org.BouncyCastle.Utilities.IO;
+
+public partial class Program
+{
+  public static void Main(string[] args)
+  {
+    // This is a placeholder for the main entry point of the application.
+    // The actual implementation will depend on the specific requirements of the project.
+    Console.WriteLine("Hello, World!");
+  }
+
+  [JSExport]
+  public static string Decrypt(string key, string msg)
+  {
+    using var input = PgpUtilities.GetDecoderStream(new MemoryStream(Encoding.UTF8.GetBytes(msg)));
+    var pgpFactory = new PgpObjectFactory(input);
+
+    var firstObject = pgpFactory.NextPgpObject();
+    if (firstObject is not PgpEncryptedDataList)
+    {
+      firstObject = pgpFactory.NextPgpObject();
+    }
+
+    PgpPrivateKey keyToUse = null;
+    PgpSecretKeyRingBundle keyRing = null;
+
+    using (var keyStream = new MemoryStream(Encoding.UTF8.GetBytes(key)))
+    {
+      keyRing = new PgpSecretKeyRingBundle(PgpUtilities.GetDecoderStream(keyStream));
+    }
+
+    var encryptedData = ((PgpEncryptedDataList)firstObject)
+      .GetEncryptedDataObjects()
+      .Cast<PgpPublicKeyEncryptedData>()
+      .FirstOrDefault(x =>
+      {
+        var key = keyRing.GetSecretKey(x.KeyId);
+        if (key != null)
+        {
+          keyToUse = key.ExtractPrivateKey([]);
+          return true;
+        }
+        return false;
+      });
+
+    if (keyToUse == null)
+    {
+      throw new PgpException("Cannot find secret key for message.");
+    }
+
+    Stream clearText = encryptedData.GetDataStream(keyToUse);
+    PgpObject message = new PgpObjectFactory(clearText).NextPgpObject();
+    string result = null;
+
+    if (message is PgpCompressedData data)
+    {
+      message = new PgpObjectFactory(data.GetDataStream()).NextPgpObject();
+    }
+
+    if (message is PgpLiteralData literalData)
+    {
+      using var outputStream = new MemoryStream();
+      Streams.PipeAll(literalData.GetInputStream(), outputStream);
+      result = Encoding.UTF8.GetString(outputStream.ToArray());
+    }
+    else
+    {
+      throw new PgpException("message is not encoded correctly.");
+    }
+
+    if (encryptedData.IsIntegrityProtected() && !encryptedData.Verify())
+    {
+      throw new Exception("message failed integrity check!");
+    }
+    return result;
+  }
+}
